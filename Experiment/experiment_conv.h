7893972f7cbd197716f8464efe991df99db2b615
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-29 19:50:51 +0100

    beam momentum offset removed

--- Experiment/experiment_conv.h
+++ Experiment/experiment_conv.h
@@ -5,7 +5,6 @@
 #define ALLRUNS int runindex=45873;runindex<=46884;runindex++
 const double p_beam_low=1.426;
 const double p_beam_hi=1.635;
-const double pbeam_measurement_offset=0.006;
 struct trigger{unsigned char number; unsigned long scaling;};
 const trigger trigger_he3_forward={.number=10,.scaling=1};
 
