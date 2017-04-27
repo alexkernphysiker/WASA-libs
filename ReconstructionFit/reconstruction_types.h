963e6187ad690152bd93b800736907dd256ed639
alexkernphysiker <kernphysiker@openmailbox.org>
2017-04-27 13:22:15 +0200

    new fit functions

--- ReconstructionFit/reconstruction_types.h
+++ ReconstructionFit/reconstruction_types.h
@@ -13,5 +13,15 @@ namespace Reconstruction{
 		                 PolynomFunc<1,0,2>,
 		Mul<      Arg<0>,PolynomFunc<1,3,2>>
 	> He3EnergyFRH2;
+	
+	typedef Add2<
+		                 PolynomFunc<1,0,2>,
+		Mul<      Arg<0>,PolynomFunc<1,3,2>>
+	> dEnergyFRH2;
+	typedef Add2<
+		                 PolynomFunc<1,0,2>,
+		Mul<      Arg<0>,PolynomFunc<1,3,2>>
+	> pEnergyFRH2;
+	
 };
 #endif
