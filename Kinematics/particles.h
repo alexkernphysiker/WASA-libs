f9a5ec5289f0afe04c88c4755b760a7ccd164cf0
alexkernphysiker <kernphysiker@openmailbox.org>
2017-05-08 16:34:26 +0200

    changes made for upgrade the analysis

--- Kinematics/particles.h
+++ Kinematics/particles.h
@@ -6,31 +6,30 @@
 #include <vector>
 #include <math_h/vectors.h>
 class Particle{
-protected:
-	Particle();
 public:
-	Particle(const Particle&source);
-	bool operator==(const Particle&source)const;
-	virtual ~Particle();
-	
-	const double mass()const;
-	const int charge()const;
-	const double E2P(const double&E)const;
-	const double P2E(const double&P)const;
-	
-	static const Particle gamma();
-	static const Particle n();
-	static const Particle p();
-	static const Particle d();
-	static const Particle he3();
-	static const Particle eta();
-	static const Particle pi0();
-	static const Particle pi_plus();
-	static const Particle pi_minus();
+    Particle();
+    Particle(const Particle&source);
+    bool operator==(const Particle&source)const;
+    virtual ~Particle();
+
+    const double mass()const;
+    const int charge()const;
+    const double E2P(const double&E)const;
+    const double P2E(const double&P)const;
+
+    static const Particle gamma();
+    static const Particle n();
+    static const Particle p();
+    static const Particle d();
+    static const Particle he3();
+    static const Particle eta();
+    static const Particle pi0();
+    static const Particle pi_plus();
+    static const Particle pi_minus();
 private:
-	Particle(double m, int c);
-	double m_mass;
-	int m_charge;
+    Particle(double m, int c);
+    double m_mass;
+    int m_charge;
 };
 // E - GeV, theta,phi - radians
 struct particle_kinematics{Particle particle; double E,theta,phi;};
