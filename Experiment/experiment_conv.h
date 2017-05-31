70926348c522813eb397722ff6f197ba3ff3496b
alexkernphysiker <kernphysiker@openmailbox.org>
2017-05-31 14:44:25 +0200

    elastic trigger

--- Experiment/experiment_conv.h
+++ Experiment/experiment_conv.h
@@ -3,13 +3,6 @@
 #ifndef AXWBNBYL
 #	define AXWBNBYL
 #include <math_h/functions.h>
-inline double NormPhi(double p){
-	const double twopi=2.0*MathTemplates::PI();
-	double phi=p;
-	while(phi<0)phi+=twopi;
-	while(phi>=twopi)phi-=twopi;
-	return phi;
-}
 #define ALLRUNS int runindex=45934;runindex<=46884;runindex++
 #define ALLMC int runindex=1;runindex<=40;runindex++
 #define CSTR(A) (const_cast<char*>(string(A).c_str()))
@@ -18,5 +11,6 @@ const double p_beam_hi=1.635;
 struct trigger{unsigned char number; unsigned long scaling;};
 const trigger trigger_he3_forward={.number=10,.scaling=1};
 const trigger trigger_gammas_central={.number=7,.scaling=10};
+const trigger trigger_elastic={.number=21,.scaling=4000};
 
 #endif
