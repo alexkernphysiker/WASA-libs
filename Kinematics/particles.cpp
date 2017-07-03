dc35715cef26819b2c983d51fb4ed5242e68eff6
alexkernphysiker <kernphysiker@openmailbox.org>
2017-07-03 18:08:33 +0200

    kinematic minor fix

--- Kinematics/particles.cpp
+++ Kinematics/particles.cpp
@@ -30,41 +30,51 @@ bool Particle::operator==(const Particle& source) const{
 	return (m_mass==source.m_mass)&&(m_charge==source.m_charge);
 }
 Particle::~Particle(){}
-const double Particle::mass() const{return m_mass;}
-const int Particle::charge() const{return m_charge;}
+const double&Particle::mass() const{return m_mass;}
+const int&Particle::charge() const{return m_charge;}
 const double Particle::E2P(const double&E) const{
 	return sqrt(pow(E+mass(),2)-pow(mass(),2));
 }
 const double Particle::P2E(const double&P) const{
 	return sqrt(pow(P,2)+pow(mass(),2))-mass();
 }
-const Particle Particle::gamma(){
-	return Particle(0.0,0);
+const Particle& Particle::gamma(){
+	static Particle res(0.0,0);
+	return res;
 }
-const Particle Particle::n(){
-	return Particle(0.93956,0);
+const Particle& Particle::n(){
+	static Particle res(0.93956,0);
+	return res;
 }
-const Particle Particle::p(){
-	return Particle(0.938272,1);
+const Particle& Particle::p(){
+	static Particle res(0.938272,1);
+	return res;
 }
-const Particle Particle::d(){
-	return Particle(1.875613,1);
+const Particle& Particle::d(){
+	static Particle res(1.875613,1);
+	return res;
 }
-const Particle Particle::he3(){
-	return Particle(2.808950,2);
+const Particle& Particle::he3(){
+	static Particle res(2.808950,2);
+	return res;
 }
-const Particle Particle::he4(){
-	return Particle(3.7274225,2);
+const Particle& Particle::he4(){
+	static Particle res(3.7274225,2);
+	return res;
 }
-const Particle Particle::eta(){
-	return Particle(0.5478,0);
+const Particle& Particle::eta(){
+	static Particle res(0.5478,0);
+	return res;
 }
-const Particle Particle::pi0(){
-	return Particle(0.1350,0);
+const Particle& Particle::pi0(){
+	static Particle res(0.1350,0);
+	return res;
 }
-const Particle Particle::pi_minus(){
-	return Particle(0.1396,-1);
+const Particle& Particle::pi_minus(){
+	static Particle res(0.1396,-1);
+	return res;
 }
-const Particle Particle::pi_plus(){
-	return Particle(0.1396,1);
+const Particle& Particle::pi_plus(){
+	static Particle res(0.1396,1);
+	return res;
 }
--- Kinematics/particles.h
+++ Kinematics/particles.h
@@ -12,21 +12,21 @@ public:
     bool operator==(const Particle&source)const;
     virtual ~Particle();
 
-    const double mass()const;
-    const int charge()const;
+    const double&mass()const;
+    const int&charge()const;
     const double E2P(const double&E)const;
     const double P2E(const double&P)const;
 
-    static const Particle gamma();
-    static const Particle n();
-    static const Particle p();
-    static const Particle d();
-    static const Particle he3();
-    static const Particle he4();
-    static const Particle eta();
-    static const Particle pi0();
-    static const Particle pi_plus();
-    static const Particle pi_minus();
+    static const Particle&gamma();
+    static const Particle&n();
+    static const Particle&p();
+    static const Particle&d();
+    static const Particle&he3();
+    static const Particle&he4();
+    static const Particle&eta();
+    static const Particle&pi0();
+    static const Particle&pi_plus();
+    static const Particle&pi_minus();
 private:
     Particle(double m, int c);
     double m_mass;
