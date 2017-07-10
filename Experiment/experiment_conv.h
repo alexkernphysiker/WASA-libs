2453d50edea80f96fee3a3ad3cf7a716bd590d0f
alexkernphysiker <kernphysiker@openmailbox.org>
2017-07-11 00:02:15 +0200

    trigger for quasielastic scattering changed

--- Experiment/experiment_conv.h
+++ Experiment/experiment_conv.h
@@ -11,6 +11,6 @@ const double p_beam_hi=1.635;
 struct trigger{unsigned char number; unsigned long scaling;};
 const trigger trigger_he3_forward={.number=10,.scaling=1};
 const trigger trigger_gammas_central={.number=7,.scaling=10};
-const trigger trigger_elastic={.number=21,.scaling=4000};
+const trigger trigger_elastic={.number=17,.scaling=4000};
 
 #endif
