7ede2b34061d1ad008062cf0545941f63cdd98ce
rundel <alexrndl@gmail.com>
2018-06-22 23:23:41 +0300

    raw analysis parameters added

--- Parameters/parameters.cpp
+++ Parameters/parameters.cpp
@@ -1,7 +1,59 @@
 // this file is distributed under 
 // GPL license
 #include <vector>
+#include <math_h/sigma.h>
 #include "parameters.h"
 using namespace std;
-void ChangedParameter(size_t index,ParameterMode mode);
-const double&getParameter(size_t index);
+using namespace MathTemplates;
+const vector<value<>> m_parameters{
+    //beam momentum correction
+    {0.0046,0.0001},
+    //3Heeta analysis
+    {0.01,0.0005},
+    //ppn analysis
+    {35.,1.},{42.,1.},//angles
+    {-18.,1.},{-9.,1.},//time
+    //3He ngamma analysis
+    {0.050,0.005},//energy threshold
+    {25.,2.},{-5.,2.},{40.,2.},//time
+    {60.,5.},//eta theta
+    {0.51,0.03},//he missing mass
+    {2.70,0.03},{3.00,0.03},//gamma missing mass
+    {0.35,0.03},{0.65,0.03},//gamma invariant mass
+    //2gamma specific
+    {-0.15,0.01},//cos theta
+    //6gamma specific
+    {0.010,0.01}//3pi0
+};
+vector<ParameterMode> m_correction;
+void init_corrections(){
+    if(m_correction.size()==0){
+        for(size_t i=0;i<m_parameters.size();i++)
+            m_correction.push_back(param_normal);
+    }
+}
+void ChangedParameter(size_t index,ParameterMode mode){
+    if(index>=m_parameters.size())
+        throw Exception<vector<value<>>,0>("Parameter index out of range");
+    init_corrections();
+    m_correction[index]=mode;
+}
+double getParameter(size_t index){
+    if(index>=m_parameters.size())
+        throw Exception<vector<value<>>,0>("Parameter index out of range");
+    init_corrections();
+    const auto&P=m_parameters[index];
+    switch(m_correction[index]){
+        case param_normal:
+            return P.val();
+        case param_up:
+            return P.max();
+        case param_down:
+            return P.min();
+        default:
+            throw Exception<vector<value<>>,1>("Parameter correction mode out of range");
+    }
+}
+size_t ParametersCount(){
+    return m_parameters.size();
+}
--- Parameters/parameters.h
+++ Parameters/parameters.h
@@ -3,7 +3,28 @@
 #ifndef _________PARAMETERS______H________
 #define _________PARAMETERS______H________
 #include <string>
-enum ParameterMode{normal,up,down};
+enum ParameterMode{param_normal,param_up,param_down};
 void ChangedParameter(size_t index,ParameterMode mode);
-const double&getParameter(size_t index);
+double getParameter(size_t index);
+size_t ParametersCount();
+//Parameter index constants
+const size_t
+pbeam_corr=0,
+he3_cut_h=pbeam_corr+1,
+ppn_th1=he3_cut_h+1,
+ppn_th2=ppn_th1+1,
+ppn_t1=ppn_th2+1,
+ppn_t2=ppn_t1+1,
+gamma_E_thr=ppn_t2+1,
+time_dt=gamma_E_thr+1,
+time_t1=time_dt+1,
+time_t2=time_t1+1,
+eta_theta_thr=time_t2+1,
+he3mm_cut=eta_theta_thr+1,
+gamma_mm_lo=he3mm_cut+1,
+gamma_mm_hi=gamma_mm_lo+1,
+gamma_im_lo=gamma_mm_hi+1,
+gamma_im_hi=gamma_im_lo+1,
+gg_theta_cut=gamma_im_hi+1,
+three_pi0=gg_theta_cut+1;
 #endif
