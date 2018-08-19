8aa00f38d1eb6a7b405e37f1defbcb7ef65c97bc
Oleksandr Rundel <oleksandr@rio3.(none)>
2018-08-19 14:09:13 +0200

    small parameters change

--- Parameters/parameters.cpp
+++ Parameters/parameters.cpp
@@ -60,7 +60,7 @@ size_t ParametersCount(){
     return m_parameters.size();
 }
 const vector<value<>> m_parameters_local{
-    {0.540,0.001},{0.555,0.001},//he3eta fit parameters
+    {0.540,0.001},{0.554,0.001},//he3eta fit parameters
     {60,5},//ppn fit
     {1,1},//bound state reaction number )
 };
